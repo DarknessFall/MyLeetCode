6209245f27f45fa2c9948f19e15e89f2e02ae43e
Bruce Jackson <zysosx@gmail.com>
2019-03-18 15:07:04 +0800

    new solution of intersection of two linked list

--- 160-IntersectionOfTwoLinkedList/IntersectionOfTwoLinkedList.c
+++ 160-IntersectionOfTwoLinkedList/IntersectionOfTwoLinkedList.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+typedef enum {
+    true = 1,
+    false = 0
+} bool;
+
 struct ListNode {
 	int val;
 	struct ListNode *next;
@@ -51,17 +56,58 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
 	return longHead;
 }
 
+struct ListNode *getIntersectionNodeNEW(struct ListNode *headA, struct ListNode *headB) {
+	if (headA == NULL || headB == NULL) {
+		return NULL;
+	}
+	
+	if (headA == headB) {
+		return headA;
+	}
+
+	struct ListNode *tailA = headA;
+	while(tailA->next != NULL){
+		tailA = tailA->next;
+	}
+	
+	tailA->next = headB;
+	struct ListNode *slow = headA;
+	struct ListNode *fast = headA;
+	bool hasCycle = false;
+	while(fast != NULL && fast->next != NULL){
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast) {
+			hasCycle = true;
+			break;
+		}
+	}
+	
+	if (hasCycle == false) {
+		return NULL;
+	}
+	
+	slow = headA;
+	while(slow != fast){
+		slow = slow->next;
+		fast = fast->next;
+	}
+	tailA->next = NULL;
+
+	return slow;
+}
+
 int main(int argc, char *argv[]) {
-	//struct ListNode e = {5, NULL};
-	//struct ListNode d = {4, &e};
-	struct ListNode c = {6, NULL};
+	struct ListNode e = {5, NULL};
+	struct ListNode d = {4, &e};
+	struct ListNode c = {6, &d};
 	struct ListNode b = {4, &c};
 	struct ListNode a = {2, &b};
 	
-	struct ListNode y = {5, NULL};
-	struct ListNode x = {1, &y};
+	struct ListNode *y = &e;
+	struct ListNode x = {1, y};
 	
-	struct ListNode *result = getIntersectionNode(&a, &x);
+	struct ListNode *result = getIntersectionNodeNEW(&a, &x);
 	if (result == NULL) {
 		printf("null");
 	} else {
